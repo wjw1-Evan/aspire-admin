1b31a4bac8132cd0330d6fcb2329d5e6a7920600
Evan <fsy_008@msn.com>
2026-03-30 23:02:04 +0800

    refactor: 将Company/Document控制器请求DTO拆分为独立文件

--- Platform.ApiService/Controllers/CompanyController.cs
+++ Platform.ApiService/Controllers/CompanyController.cs
@@ -449,36 +449,3 @@ public class CompanyController : BaseApiController
 
     #endregion
 }
-
-/// <summary>
-/// 切换企业请求
-/// </summary>
-public class SwitchCompanyRequest
-{
-    /// <summary>
-    /// 目标企业ID
-    /// </summary>
-    public string TargetCompanyId { get; set; } = string.Empty;
-}
-
-/// <summary>
-/// 更新成员角色请求
-/// </summary>
-public class UpdateMemberRolesRequest
-{
-    /// <summary>
-    /// 角色ID列表
-    /// </summary>
-    public List<string> RoleIds { get; set; } = new();
-}
-
-/// <summary>
-/// 设置管理员请求
-/// </summary>
-public class SetAdminRequest
-{
-    /// <summary>
-    /// 是否设置为管理员
-    /// </summary>
-    public bool IsAdmin { get; set; }
-}

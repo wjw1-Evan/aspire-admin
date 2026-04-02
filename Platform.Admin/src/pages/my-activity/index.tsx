abcf15eb6f335fc5dac74dab84fb6492f382ac0d
Evan <fsy_008@163.com>
2026-04-02 20:14:06 +0800

    fix(admin): 添加 SearchOutlined 导入

--- Platform.Admin/src/pages/my-activity/index.tsx
+++ Platform.Admin/src/pages/my-activity/index.tsx
@@ -14,6 +14,7 @@ import {
   ThunderboltOutlined,
   ReloadOutlined,
   DashboardOutlined,
+  SearchOutlined,
 } from '@ant-design/icons';
 import React, { useRef, useState, useEffect, useCallback, useMemo } from 'react';
 import { useIntl } from '@umijs/max';

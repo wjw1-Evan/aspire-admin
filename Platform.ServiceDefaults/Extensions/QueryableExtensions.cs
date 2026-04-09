001dc9e19238cf4ff58a3f436fb09269091bc1ee
Evan <fsy_008@163.com>
2026-04-09 20:03:03 +0800

    fix: 仅搜索string类型，避免MongoDB Contains报错

--- Platform.ServiceDefaults/Extensions/QueryableExtensions.cs
+++ Platform.ServiceDefaults/Extensions/QueryableExtensions.cs
@@ -4,28 +4,6 @@ namespace Platform.ServiceDefaults.Extensions;
 
 public static class QueryableExtensions
 {
-    private static readonly HashSet<string> NonSearchableTypes = new()
-    {
-        typeof(object).FullName!,
-        typeof(Dictionary<,>).FullName!,
-        typeof(IDictionary<,>).FullName!,
-        "System.Collections.Generic.Dictionary`2",
-        "System.Collections.IDictionary"
-    };
-
-    private static bool IsSearchableType(Type type)
-    {
-        if (type.IsArray || type.IsGenericType)
-        {
-            var genType = type.GetGenericTypeDefinition().FullName ?? "";
-            if (NonSearchableTypes.Contains(genType) || type.FullName?.StartsWith("System.Collections.Generic.List") == false)
-            {
-                return type == typeof(string);
-            }
-        }
-        return type == typeof(string);
-    }
-
     public static PagedResult<T> ToPagedList<T>(
         this IQueryable<T> query,
         Models.PageParams? pageParams)
@@ -37,7 +15,7 @@ public static class QueryableExtensions
             var props = typeof(T).GetProperties()
                 .Where(x => x.CanRead 
                     && x.GetIndexParameters().Length == 0 
-                    && IsSearchableType(x.PropertyType))
+                    && x.PropertyType == typeof(string))
                 .Select(x => x.Name)
                 .ToList();
 

baf0826ddad495f92ae9de13d56574863a317cc8
Evan <fsy_008@163.com>
2026-04-27 20:26:21 +0800

    fix: 调整 breakpoints 支持更大宽度

--- Platform.Admin/src/pages/dashboard/components/DashboardDesigner.tsx
+++ Platform.Admin/src/pages/dashboard/components/DashboardDesigner.tsx
@@ -254,7 +254,7 @@ const DashboardDesigner: React.FC<DashboardDesignerProps> = ({ dashboardId, onPr
               className="dashboard-designer-grid"
               width={containerWidth > 0 ? containerWidth : window.innerWidth}
               layouts={layouts}
-              breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
+              breakpoints={{ lg: 9999, md: 996, sm: 768, xs: 480, xxs: 0 }}
               cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
               rowHeight={40}
               onLayoutChange={(currentLayout: LayoutItem[]) => handleLayoutChange(currentLayout)}
